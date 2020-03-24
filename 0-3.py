fd6374f083c20ab34dc6c0078ecc3377016bde95
sahagunjaime <48419251+sahagunjaime@users.noreply.github.com>
2020-03-23 23:15:40 -0700

    Update 0-3.py

--- 0-3.py
+++ 0-3.py
@@ -9,10 +9,8 @@ L= np.log(2)/170
 N_0=29250
 
 plt.title('Ba137 Decay')
-plt.xlabel('time (s)')
+plt.xlabel('time [s]')
 plt.ylabel('N')
-#plt.xlim(0, 500)
-#plt.ylim(0, 30000)
 
 plt.grid(color='black', linestyle='-', linewidth=.1)
 plt.plot(t, N, color='black', label='Ba137 data')
@@ -20,4 +18,4 @@ plt.plot(t, N_0*np.exp(-L*t), color='red', label='approx.')
 plt.legend(loc='best')
 
 figure = Figure(figsize=(6, 5))
-plt.savefig('/Users/sahagunjaime/Desktop/prob3.png')
+plt.show()
