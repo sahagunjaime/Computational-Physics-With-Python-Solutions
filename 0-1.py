838b467c4b694132526a0b0a2affec9b49b65460
sahagunjaime <48419251+sahagunjaime@users.noreply.github.com>
2020-03-23 23:46:39 -0700

    Add files via upload

---
+++ 0-1.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+
+f,m1,m2 = np.loadtxt('/Users/sahagunjaime/Desktop/microphone.txt', unpack=True)
+
+plt.xlabel('Frequency [Hz]')
+plt.ylabel(r'Amplitude Ratio [$Mic_2/Mic_1$]')
+#plt.xlim(0, 5)
+#plt.ylim(-.2, .5)
+
+plt.plot(f, m2/m1, color='red')
+
+plt.show()
